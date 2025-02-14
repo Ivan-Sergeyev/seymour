f62da4ffd753d32f26aac25a260987d0f28e4eb1
madvorak <martin.dvorak@matfyz.cz>
2025-02-14 16:22:10 +0100

    auxiliary lemmas wip

--- Seymour/ForMathlib/MatrixLI.lean
+++ Seymour/ForMathlib/MatrixLI.lean
@@ -1,17 +1,16 @@
 import Seymour.Basic
 
-#check Matrix.linearIndependent_rows_of_invertible
-#check Matrix.Nondegenerate
+-- #check Matrix.linearIndependent_rows_of_invertible
+-- #check Matrix.Nondegenerate
 
+variable {X Y F : Type} [DecidableEq X] [Fintype X] [Field F]
 
-lemma Matrix.linearIndendent_iff_exists_submatrix_unit {X Y F : Type} [DecidableEq X] [Fintype X] [Field F]
-    (A : Matrix X Y F) :
+lemma Matrix.linearIndendent_iff_exists_submatrix_unit (A : Matrix X Y F) :
     LinearIndependent F A ↔ ∃ f : X → Y, IsUnit (A.submatrix id f) := by
   sorry
 
-lemma Matrix.linearIndendent_iff_exists_submatrix_det {X Y F : Type} [DecidableEq X] [Fintype X] [Field F]
-    (A : Matrix X Y F) :
+lemma Matrix.linearIndendent_iff_exists_submatrix_det (A : Matrix X Y F) :
     LinearIndependent F A ↔ ∃ f : X → Y, (A.submatrix id f).det ≠ 0 := by
   convert A.linearIndendent_iff_exists_submatrix_unit
-  rw [Matrix.isUnit_iff_isUnit_det]
-  exact isUnit_iff_ne_zero.symm
+  convert isUnit_iff_ne_zero.symm
+  apply Matrix.isUnit_iff_isUnit_det
--- Seymour/Matroid/Notions/Regularity.lean
+++ Seymour/Matroid/Notions/Regularity.lean
@@ -17,9 +17,94 @@ variable {α : Type}
 def Matroid.IsRegular (M : Matroid α) : Prop :=
   ∃ X Y : Set α, ∃ A : Matrix X Y ℚ, A.IsTotallyUnimodular ∧ (VectorMatroid.mk X Y A).toMatroid = M
 
-private lemma todo [DecidableEq α] [Fintype α] {A : Matrix α α ℚ} (hA : ∀ i j, A i j ∈ Set.range SignType.cast) :
+lemma Matrix.det_coe_ℤ_ℚ [DecidableEq α] [Fintype α] (A : Matrix α α ℤ) :
+    ((A.det : ℤ) : ℚ) = (A.map (fun (a : ℤ) => (a : ℚ))).det := by
+  simp [Matrix.det_apply]
+  congr
+  ext p
+  if h1 : Equiv.Perm.sign p = 1 then
+    simp [h1]
+  else
+    simp [Int.units_ne_iff_eq_neg.→ h1]
+-- TODO can these two lemmas be generalized?
+lemma Matrix.det_coe_ℤ_Z2 [DecidableEq α] [Fintype α] {A : Matrix α α ℤ} :
+    ((A.det : ℤ) : Z2) = (A.map (fun (a : ℤ) => (a : Z2))).det := by
+  simp [Matrix.det_apply]
+  congr
+  ext p
+  if h1 : Equiv.Perm.sign p = 1 then
+    simp [h1]
+  else
+    simp [Int.units_ne_iff_eq_neg.→ h1]
+
+private lemma todoZ [DecidableEq α] [Fintype α] (A : Matrix α α ℤ)
+    (hA : ∀ i j, A i j ∈ Set.range SignType.cast) (hA' : A.det ∈ Set.range SignType.cast) :
+    A.det = (0 : ℤ) ↔ (Matrix.of (if A · · = 0 then 0 else 1)).det = (0 : Z2) := by
+  have h0 : A.det = (0 : ℤ) ↔ (A.det : Z2) = (0 : Z2)
+  · obtain ⟨s, hs⟩ := hA'
+    cases s with
+    | zero => exact ⟨congrArg Int.cast, fun _ => hs.symm⟩
+    | pos | neg =>
+      simp at hs
+      rw [←hs]
+      simp
+  rw [h0, A.det_coe_ℤ_Z2]
+  constructor -- TODO eliminate repeated code below
+  · intro foo
+    convert foo
+    ext i j
+    simp
+    if hij0 : A i j = 0 then
+      simp_all
+    else
+      simp [*]
+      symm
+      apply Fin2_eq_1_of_ne_0
+      intro contr
+      apply hij0
+      obtain ⟨s, hs⟩ := hA i j
+      cases s with
+      | zero => exact hs.symm
+      | pos | neg =>
+        exfalso
+        simp at hs
+        rw [←hs] at contr
+        simp at contr
+  · intro foo
+    convert foo
+    ext i j
+    simp
+    if hij0 : A i j = 0 then
+      simp_all
+    else
+      simp [*]
+      apply Fin2_eq_1_of_ne_0
+      intro contr
+      apply hij0
+      obtain ⟨s, hs⟩ := hA i j
+      cases s with
+      | zero => exact hs.symm
+      | pos | neg =>
+        exfalso
+        simp at hs
+        rw [←hs] at contr
+        simp at contr
+
+private lemma todo [DecidableEq α] [Fintype α] {A : Matrix α α ℚ}
+    (hA : ∀ i j, A i j ∈ Set.range SignType.cast) (hA' : A.det ∈ Set.range SignType.cast) :
     A.det = (0 : ℚ) ↔ (Matrix.of (if A · · = 0 then 0 else 1)).det = (0 : Z2) := by
-  sorry
+  have key : (((Matrix.of (if A · · = 0 then 0 else 1)).det : ℤ) : ℚ) = A.det
+  · convert (Matrix.of (if A · · = 0 then 0 else 1)).det_coe_ℤ_ℚ
+    ext i j
+    simp
+    obtain ⟨s, hs⟩ := hA i j
+    cases s with
+    | zero => simp_all only [Set.mem_range, SignType.zero_eq_zero, SignType.coe_zero, ite_true]
+    | pos => aesop
+    | neg => sorry -- TODO does not work this way; preserving `-1` is necessary now.
+  convert todoZ (Matrix.of (if A · · = 0 then 0 else 1)) (by sorry) (by sorry)
+  · simp [←key]
+  · simp
 
 /-- Every regular matroid is binary. -/
 lemma Matroid.IsRegular.isBinary [DecidableEq α] {M : Matroid α} [Finite M.E] (hM : M.IsRegular) :
@@ -35,9 +120,10 @@ lemma Matroid.IsRegular.isBinary [DecidableEq α] {M : Matroid α} [Finite M.E] 
       rw [Matrix.linearIndendent_iff_exists_submatrix_det] at hA' ⊢ <;>
       obtain ⟨f, hAf⟩ := hA' <;> use f <;> intro contr <;>
       rw [Matrix.transpose_submatrix, Matrix.submatrix_submatrix, Function.comp_id, Function.id_comp] at hAf contr <;>
-      apply hAf <;> have hAf' := (hA.transpose.submatrix hIY.elem f).apply
-  · rwa [todo hAf'] at contr
-  · rwa [todo hAf']
+      apply hAf <;> have hAT := hA.transpose <;> have hAf' := (hAT.submatrix hIY.elem f).apply <;>
+      rw [Matrix.isTotallyUnimodular_iff_fintype] at hAT
+  · rwa [todo hAf' (hAT ..)] at contr
+  · rwa [todo hAf' (hAT ..)]
 
 /-- Every regular matroid has a standard binary representation. -/
 lemma Matroid.IsRegular.isBinaryStd [DecidableEq α] {M : Matroid α} [Finite M.E] (hM : M.IsRegular) :
