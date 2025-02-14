2f5cd38c9236d5f7bcab65d37a0ae0884e4f4893
madvorak <martin.dvorak@matfyz.cz>
2025-02-14 16:28:13 +0100

    dox for Matrix.linearIndendent_iff_exists_submatrix_unit

--- Seymour/ForMathlib/MatrixLI.lean
+++ Seymour/ForMathlib/MatrixLI.lean
@@ -5,10 +5,12 @@ import Seymour.Basic
 
 variable {X Y F : Type} [DecidableEq X] [Fintype X] [Field F]
 
+/-- Rows of a matrix are linearly independent iff the matrix contains a nonsigular square submatrix of full height. -/
 lemma Matrix.linearIndendent_iff_exists_submatrix_unit (A : Matrix X Y F) :
     LinearIndependent F A ↔ ∃ f : X → Y, IsUnit (A.submatrix id f) := by
   sorry
 
+/-- Rows of a matrix are linearly independent iff the matrix contains a square submatrix of full height with nonzero det. -/
 lemma Matrix.linearIndendent_iff_exists_submatrix_det (A : Matrix X Y F) :
     LinearIndependent F A ↔ ∃ f : X → Y, (A.submatrix id f).det ≠ 0 := by
   convert A.linearIndendent_iff_exists_submatrix_unit
