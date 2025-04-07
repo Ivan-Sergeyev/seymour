de94c52e6b58608a06bb3fb9535475901490cbba
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-04-07 12:57:39 +0200

    the G x G submatrix is the identity matrix

--- Seymour/Matroid/Constructors/StandardRepresentation.lean
+++ Seymour/Matroid/Constructors/StandardRepresentation.lean
@@ -275,6 +275,14 @@ lemma VectorMatroid.exists_standardRepr_isBase [Field R] {G : Set α}
   · simp
   ext I
   · aesop
+  have hAG : ∀ a : G, (A · (hGY.elem a)) = Finsupp.single a 1
+  · intro i
+    simp [A, B, ←B.repr_self i]
+  have hA1 : A.submatrix id hGY.elem = 1
+  · ext i j
+    simp only [Matrix.submatrix_apply, id]
+    rw [congr_fun (hAG j) i]
+    by_cases hij : i = j <;> simp [Matrix.one_apply, Finsupp.single_eq_set_indicator, hij]
   have hGYY : G ∪ M.Y = M.Y := Set.union_eq_self_of_subset_left hGY
   simp only [StandardRepr.toMatroid_indep_iff_elem', VectorMatroid.toMatroid_indep_iff_elem,
     Matrix.transpose_submatrix, Set.union_diff_self]
@@ -283,24 +291,6 @@ lemma VectorMatroid.exists_standardRepr_isBase [Field R] {G : Set α}
   · use hGYY ▸ hI
     sorry
   · use Set.subset_union_of_subset_right hI G
-    suffices : LinearIndepOn R Aᵀ hI.elem.range
-    · clear hRAI
-      have hGYGY : G ∪ M.Y \ G = M.Y := Set.union_diff_cancel' (fun _ => id) hGE
-      have hGYGY' : (G ∪ M.Y \ G).Elem = M.Y.Elem := congr_arg Set.Elem hGYGY
-      let hIG := (Iff.of_eq (congr_arg (I ⊆ ·) Set.union_diff_self)).← (Set.subset_union_of_subset_right hI G)
-      show LinearIndepOn R _ hIG.elem.range
-      unfold LinearIndepOn
-      convert this.comp (fun a : hIG.elem.range => ⟨hGYGY ▸ a.val, by have ⟨i, hi⟩ := a.property; sorry⟩) sorry
-      rename_i i
-      simp only [Matrix.transpose_submatrix, Function.comp_apply]
-      classical
-      -- let e : G.Elem ⊕ (M.Y \ G).Elem ≃ M.Y.Elem := hGY.myEquiv
-      -- let q := e i.val.toSum
-      cases hi : i.val.toSum with
-      | inl i₁ =>
-        sorry
-      | inr i₂ =>
-        sorry
     sorry
 
 /-- Every vector matroid has a standard representation. -/
