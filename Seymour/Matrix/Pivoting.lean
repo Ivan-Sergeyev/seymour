3b71689952d2d6740542710c36caff3bd6c59a8b
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-04-09 15:27:58 +0200

    Matrix.shortTableauPivot_elem_of_ne_ne should have the matrix argument explicit

--- Seymour/Matrix/Pivoting.lean
+++ Seymour/Matrix/Pivoting.lean
@@ -20,12 +20,13 @@ def Matrix.shortTableauPivot [DivisionRing F] [DecidableEq X] [DecidableEq Y] (A
       else
         A i j - A i y * A x j / A x y
 
-lemma Matrix.shortTableauPivot_elem_of_ne_ne [DivisionRing F] [DecidableEq X] [DecidableEq Y] {A : Matrix X Y F}
-    {x : X} {y : Y} {i : X} {j : Y} (hix : i ≠ x) (hjx : j ≠ y) :
+lemma Matrix.shortTableauPivot_elem_of_ne_ne [DivisionRing F] [DecidableEq X] [DecidableEq Y]
+    (A : Matrix X Y F) {x : X} {y : Y} {i : X} {j : Y} (hix : i ≠ x) (hjx : j ≠ y) :
     A.shortTableauPivot x y i j = A i j - A i y * A x j / A x y := by
   simp [Matrix.shortTableauPivot, hix, hjx]
 
-lemma Matrix.shortTableauPivot_row_pivot [DivisionRing F] [DecidableEq X] [DecidableEq Y] (A : Matrix X Y F) (x : X) (y : Y) :
+lemma Matrix.shortTableauPivot_row_pivot [DivisionRing F] [DecidableEq X] [DecidableEq Y]
+    (A : Matrix X Y F) (x : X) (y : Y) :
     A.shortTableauPivot x y x =
     (fun j : Y => if j = y then 1 / A x y else A x j / A x y) := by
   ext
