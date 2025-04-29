a81fff8b604bff2a95a7520eb1a6adead1eeb033
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-04-29 08:16:42 +0200

    Matrix.Z2_2x2_nonsingular_form proved

--- Seymour/Matroid/Operations/Sum3.lean
+++ Seymour/Matroid/Operations/Sum3.lean
@@ -125,37 +125,28 @@ section CanonicalSigning
 /-- Lemma 10 (motivates the difference between the definition of 3-sum in our implementation and in Truemper's book). -/
 private lemma Matrix.Z2_2x2_nonsingular_form (Q : Matrix (Fin 2) (Fin 2) Z2) (hQ : IsUnit Q) :
     ∃ f : Fin 2 ≃ Fin 2, ∃ g : Fin 2 ≃ Fin 2, Q.submatrix f g = 1 ∨ Q.submatrix f g = !![1, 1; 0, 1] := by
-  -- obtain ⟨i₀, hi₀⟩ : ∃ i : Fin 2, Q i 0 = 1
-  -- · sorry
-  -- obtain ⟨i₁, hi₁⟩ : ∃ i : Fin 2, Q i 0 = 1
-  -- · sorry
-  -- obtain ⟨j₀, hj₀⟩ : ∃ j : Fin 2, Q 0 j = 1
-  -- · sorry
-  -- obtain ⟨j₁, hj₁⟩ : ∃ j : Fin 2, Q 1 j = 1
-  -- · sorry
-  -- have hQ1 : Q ≠ !![1, 1; 1, 1]
-  -- · sorry
+  rw [Matrix.isUnit_iff_isUnit_det, Matrix.det_fin_two, isUnit_iff_eq_one] at hQ
   let eᵢ : Fin 2 ≃ Fin 2 := Equiv.refl (Fin 2)
-  let eₛ : Fin 2 ≃ Fin 2 := Equiv.ofBijective ![1, 0] sorry
+  let eₛ : Fin 2 ≃ Fin 2 := Equiv.ofBijective ![1, 0] (by decide)
   by_cases hQ₀₀ : Q 0 0 = 0 <;> by_cases hQ₀₁ : Q 0 1 = 0 <;> by_cases hQ₁₀ : Q 1 0 = 0 <;> by_cases hQ₁₁ : Q 1 1 = 0
   · -- `!![0, 0; 0, 0]`
     exfalso
-    sorry
+    simp_all
   · -- `!![0, 0; 0, 1]`
     exfalso
-    sorry
+    simp_all
   · -- `!![0, 0; 1, 0]`
     exfalso
-    sorry
+    simp_all
   · -- `!![0, 0; 1, 1]`
     exfalso
-    sorry
+    simp_all
   · -- `!![0, 1; 0, 0]`
     exfalso
-    sorry
+    simp_all
   · -- `!![0, 1; 0, 1]`
     exfalso
-    sorry
+    simp_all
   · -- `!![0, 1; 1, 0]`
     use eₛ, eᵢ
     left
@@ -168,7 +159,7 @@ private lemma Matrix.Z2_2x2_nonsingular_form (Q : Matrix (Fin 2) (Fin 2) Z2) (hQ
     fin_cases i <;> fin_cases j <;> simp [*, eᵢ, eₛ, fin2_eq_1_of_ne_0]
   · -- `!![1, 0; 0, 0]`
     exfalso
-    sorry
+    simp_all
   · -- `!![1, 0; 0, 1]`
     use eᵢ, eᵢ
     left
@@ -176,7 +167,7 @@ private lemma Matrix.Z2_2x2_nonsingular_form (Q : Matrix (Fin 2) (Fin 2) Z2) (hQ
     fin_cases i <;> fin_cases j <;> simp [*, eᵢ, eₛ, fin2_eq_1_of_ne_0]
   · -- `!![1, 0; 1, 0]`
     exfalso
-    sorry
+    simp_all
   · -- `!![1, 0; 1, 1]`
     use eₛ, eₛ
     right
@@ -184,7 +175,7 @@ private lemma Matrix.Z2_2x2_nonsingular_form (Q : Matrix (Fin 2) (Fin 2) Z2) (hQ
     fin_cases i <;> fin_cases j <;> simp [*, eᵢ, eₛ, fin2_eq_1_of_ne_0]
   · -- `!![1, 1; 0, 0]`
     exfalso
-    sorry
+    simp_all
   · -- `!![1, 1; 0, 1]`
     use eᵢ, eᵢ
     right
@@ -197,7 +188,7 @@ private lemma Matrix.Z2_2x2_nonsingular_form (Q : Matrix (Fin 2) (Fin 2) Z2) (hQ
     fin_cases i <;> fin_cases j <;> simp [*, eᵢ, eₛ, fin2_eq_1_of_ne_0]
   · -- `!![1, 1; 1, 1]`
     exfalso
-    sorry
+    simp_all [fin2_eq_1_of_ne_0]
 
 -- converts a TU signing of a summand of 3-sum to a canonical TU signing
 private def Matrix.toCanonicalSigning {X Y : Set α} (Q : Matrix X Y ℚ) (x₀ x₁ x' : X) (y₀ y₁ y' : Y) :
