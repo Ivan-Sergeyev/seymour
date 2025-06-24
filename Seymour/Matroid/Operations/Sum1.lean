3bce763e4af2b01e8160a9085cba6c677c6b6d14
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-06-24 17:27:29 +0200

    1-sum vs disjoinSum wip

--- Seymour/Matroid/Operations/Sum1.lean
+++ Seymour/Matroid/Operations/Sum1.lean
@@ -49,22 +49,97 @@ instance Matroid.Is1sumOf.finS {M Mₗ Mᵣ : Matroid α} (hM : M.Is1sumOf Mₗ 
   obtain ⟨_, Sₗ, Sᵣ, _, _, _, _, _, _, _, rfl, _⟩ := hM
   exact Finite.Set.finite_union Sₗ.X Sᵣ.X
 
+private lemma standardRepr1sumComposition_eq_disjointSum_aux_full {Xₗ Yₗ Xᵣ Yᵣ : Set α}
+    [∀ a, Decidable (a ∈ Xₗ)] [∀ a, Decidable (a ∈ Xᵣ)] [∀ a, Decidable (a ∈ Yₗ)] [∀ a, Decidable (a ∈ Yᵣ)]
+    (Aₗ : Matrix Xₗ Yₗ Z2) (Aᵣ : Matrix Xᵣ Yᵣ Z2) (hYY : Yₗ ⫗ Yᵣ) :
+    (⊞ Aₗ 0 0 Aᵣ).toMatrixUnionUnion.toMatroid = Matroid.disjointSum Aₗ.toMatroid Aᵣ.toMatroid hYY := by
+  ext I hI
+  · simp
+  sorry
+
 /-- Matroid constructed from a valid 1-sum of binary matroids is the same as disjoint sum of matroids constructed from them. -/
 lemma standardRepr1sumComposition_eq_disjointSum {Sₗ Sᵣ : StandardRepr α Z2} {hXY : Sₗ.X ⫗ Sᵣ.Y} {hYX : Sₗ.Y ⫗ Sᵣ.X}
     (valid : (standardRepr1sumComposition hXY hYX).snd) :
     (standardRepr1sumComposition hXY hYX).fst.toMatroid = Matroid.disjointSum Sₗ.toMatroid Sᵣ.toMatroid (by
       simp [StandardRepr.toMatroid, StandardRepr.toFull, Set.disjoint_union_left, Set.disjoint_union_right]
       exact ⟨⟨valid.left, hYX⟩, ⟨hXY, valid.right⟩⟩) := by
+  convert standardRepr1sumComposition_eq_disjointSum_aux_full Sₗ.toFull Sᵣ.toFull (by aesop)
+  have hXXYY : (Sₗ.X ∪ Sᵣ.X) ∪ (Sₗ.Y ∪ Sᵣ.Y) = (Sₗ.X ∪ Sₗ.Y) ∪ (Sᵣ.X ∪ Sᵣ.Y)
+  · tauto_set
   ext I hI
-  · simp only [StandardRepr.toMatroid_E, Set.mem_union, Matroid.disjointSum_ground_eq, standardRepr1sumComposition]
-    tauto
-  · simp only [StandardRepr.toMatroid_indep_iff, Matroid.disjointSum_indep_iff, StandardRepr.toMatroid_E,
-      Set.inter_subset_right, exists_true_left]
-    constructor
-    <;> intro linearlyI
-    · sorry
-    · use hI
-      sorry
+  · simp [standardRepr1sumComposition]
+    tauto_set
+  have hXX : Sₗ.X ⫗ Sᵣ.X := sorry -- Is it needed? Then add as assumption!
+  have hYY : Sₗ.Y ⫗ Sᵣ.Y := sorry -- Is it needed? Then add as assumption!
+  have hXYₗ := Sₗ.hXY
+  have hXYᵣ := Sᵣ.hXY
+  rw [Matrix.toMatroid_indep_iff_submatrix, StandardRepr.toMatroid_indep_iff_submatrix]
+  constructor
+  <;> intro ⟨hI, hISS⟩
+  <;> use hXXYY ▸ hI
+  · convert hISS
+    ext i j
+    cases hj : j.toSum with
+    | inl jₗ =>
+      simp [standardRepr1sumComposition, matrix1sumComposition, StandardRepr.toFull_def, hj]
+      if hiXₗ : i.val ∈ Sₗ.X then
+        simp_all [Matrix.toMatrixUnionUnion]
+        generalize_proofs hhi
+        if hji : j = ⟨i.val, hhi⟩ then
+          have hjₗ : jₗ = ⟨i, hiXₗ⟩
+          · simp_all
+          rw [hji, hjₗ, Matrix.one_apply_eq, Matrix.one_apply_eq]
+        else
+          have hjₗ : jₗ ≠ ⟨i, hiXₗ⟩
+          · intro contr
+            apply hji
+            apply SetCoe.ext
+            have hjj : j.val = jₗ.val
+            · have := toSum_toUnion j
+              simp_all
+            simp [hjj, congr_arg Subtype.val contr]
+          rw [Matrix.one_apply_ne hji, Matrix.one_apply_ne hjₗ]
+      else
+        have hiXᵣ : i.val ∈ Sᵣ.X
+        · sorry
+        simp_all [Matrix.toMatrixUnionUnion]
+        generalize_proofs hiXYXY hiXX
+        dsimp [standardRepr1sumComposition] at hiXX
+        have hiₗ : i.val ∈ Sₗ.X ∪ Sₗ.Y
+        · sorry
+        if hji : j = ⟨i.val, hiXX⟩ then
+          rw [hji, Matrix.one_apply_eq]
+          sorry
+        else
+          sorry
+    | inr jᵣ => sorry
+  · convert hISS
+    ext i j
+    cases hj : j.toSum with
+    | inl jₗ =>
+      simp [standardRepr1sumComposition, matrix1sumComposition, StandardRepr.toFull_def, hj]
+      if hiXₗ : i.val ∈ Sₗ.X then
+        simp_all [Matrix.toMatrixUnionUnion]
+        generalize_proofs hhi
+        if hji : j = ⟨i.val, hhi⟩ then
+          have hjₗ : jₗ = ⟨i, hiXₗ⟩
+          · simp_all
+          rw [hji, hjₗ, Matrix.one_apply_eq, Matrix.one_apply_eq]
+        else
+          have hjₗ : jₗ ≠ ⟨i, hiXₗ⟩
+          · intro contr
+            apply hji
+            apply SetCoe.ext
+            have hjj : j.val = jₗ.val
+            · have := toSum_toUnion j
+              simp_all
+            simp [hjj, congr_arg Subtype.val contr]
+          rw [Matrix.one_apply_ne hji, Matrix.one_apply_ne hjₗ]
+      else
+        have hiXᵣ : i.val ∈ Sᵣ.X
+        · sorry
+        sorry
+    | inr jᵣ => sorry
 
 /-- A valid 1-sum of binary matroids is commutative. -/
 lemma standardRepr1sumComposition_comm {Sₗ Sᵣ : StandardRepr α Z2} {hXY : Sₗ.X ⫗ Sᵣ.Y} {hYX : Sₗ.Y ⫗ Sᵣ.X}
