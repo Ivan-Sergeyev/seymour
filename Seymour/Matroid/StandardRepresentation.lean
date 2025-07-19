b5d7cc6abbce0204a66d05ecc4a8258cfe21641f
Martin Dvorak <martin.dvorak@matfyz.cz>
2025-07-19 12:41:48 +0200

    wip

--- Seymour/Matroid/StandardRepresentation.lean
+++ Seymour/Matroid/StandardRepresentation.lean
@@ -720,20 +720,40 @@ lemma Matrix.exists_standardRepr_isBase_isTotallyUnimodular [Field R] {X Y G : S
   else
     cases j.property <;> simp [*, StandardRepr.toFull] at hjG ⊢
 
+omit [DecidableEq α] in
 private def List.seqApply (l : List (α → α)) : α → α :=
   l.foldl Function.comp id
+
+@[app_unexpander List.seqApply]
+private def List.seqApply_unexpand : Lean.PrettyPrinter.Unexpander
+  | `($_ $l) => `($(l).$(Lean.mkIdent `seqApply))
+  | _ => throw ()
+
+omit [DecidableEq α] in
+private lemma seqApply_empty (a : α) : [].seqApply a = a := by
+  rfl
+
+omit [DecidableEq α] in
+private lemma seqApply_singleton (f : α → α) (a : α) : [f].seqApply a = f a := by
+  rfl
 
 lemma Matrix.exists_standardRepr_isBase_isTotallyUnimodular_strong [Field R] {X Y G : Set α}
     (A : Matrix X Y R) (hAG : A.toMatroid.IsBase G) (hA : A.IsTotallyUnimodular) :
     ∃ S : StandardRepr α R, S.X = G ∧ S.toMatroid = A.toMatroid ∧ S.B.IsTotallyUnimodular := by
-  have hYGY : Y \ G ⊆ Y := Set.diff_subset
-  use ⟨G, Y \ G, Set.disjoint_sdiff_right,
-    Matrix.of <| fun i : G.Elem => fun j : (Y \ G).Elem =>
-      let D := { x : X | A x (hYGY.elem j) ≠ 0 }
-      have finiteD : Finite D := sorry
+  let A' : Matrix X Y R :=
+    Matrix.of <| fun i : X.Elem => fun j : Y.Elem =>
+      let D := { x : X | A x j ≠ 0 }
+      have finiteD : Finite D := by
+        by_contra hD
+        have indep : A.toMatroid.Indep (Subtype.val '' (i ᕃ D))
+        · sorry
+        have dep : ¬ A.toMatroid.Indep (Subtype.val '' (i ᕃ D))
+        · sorry
+        exact dep indep
       have fintypeD : Fintype D := Set.Finite.fintype finiteD
-      let A' : Matrix X Y R := (D.toFinset.toList.map (fun N : Matrix X Y R => N.longTableauPivot · (hYGY.elem j))).seqApply A
-      sorry,
+      (D.toFinset.toList.map (fun N : Matrix X Y R => N.longTableauPivot · j)).seqApply A i j
+  have hYGY : Y \ G ⊆ Y := Set.diff_subset
+  use ⟨G, Y \ G, Set.disjoint_sdiff_right, sorry,
     (Classical.propDecidable <| · ∈ G), (Classical.propDecidable <| · ∈ Y \ G)⟩
   sorry
 
