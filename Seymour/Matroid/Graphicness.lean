f019b4e5349b4c8c9a84e7d2657b7914e60067cc
Cameron R <68977642+cappucher@users.noreply.github.com>
2025-12-27 09:46:43 -1000

    Update Seymour/Matroid/Graphicness.lean
    Co-authored-by: Martin Dvořák <martin.dvorak@matfyz.cz>

--- Seymour/Matroid/Graphicness.lean
+++ Seymour/Matroid/Graphicness.lean
@@ -220,7 +220,7 @@ private lemma rth_rowSpace_stdMat
   [Fintype (Sum X Y)] :
   orth (rowSpace (1 ◫ B))
     =
-  Submodule.span (Z2)
+  Submodule.span Z2
     (fun b => Sum.elim (b ᵥ* Bᵀ) b).range :=
 by
   sorry
